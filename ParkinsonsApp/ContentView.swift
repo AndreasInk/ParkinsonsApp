801abd7476187455f8374649d7b96edb8a3b4809
Andreas Ink <andreasink17@gmail.com>
2021-05-04 18:11:26 -0400

    Initial Commit

---
+++ ParkinsonsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParkinsonsApp
+//
+//  Created by Andreas on 5/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
